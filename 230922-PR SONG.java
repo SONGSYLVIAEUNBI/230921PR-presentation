013c85c38ec4f3bd20e376a922bf5f24dc0493c8
SONGSYLVIAEUNBI <hello221eunbi@yahoo.co.jp>
2023-09-22 12:41:34 +0900

    first commit

---
+++ 230922-PR SONG.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+public class Main{
+  public static void main(String[]args){
+    Random rnd=new Random();
+    int[]lottoNumbers=new int[6];
+    for(int i=0;i<6;i++){
+        int randomNumber=rnd.nextInt(43)+1;
+        for(int j=0;j<i;j++){
+            if(lottoNumbers[j]==randomNumber){
+                randomNumber=rnd.nextInt(43)+1;
+                j=-1;
+            }
+        }
+        lottoNumbers[i]=randomNumber;
+    }
+    java.util.Arrays.sort(lottoNumbers);
+    System.out.println("今回のロト6予測番号はこちらです。");
+    String word="";
+        for(int number:lottoNumbers){
+        word=word+"["+(number+"")+"]";
+        }
+    System.out.println(word);
+    }
+}
